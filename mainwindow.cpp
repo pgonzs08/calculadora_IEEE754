225385a2f5e6c15e8618731a1d90157fac1011d3
Pablo González Santamarta <pablogonzalezsantamarta@MacBook-Pro-de-Pablo.local>
2022-05-06 17:16:47 +0200

    multiplicacion y suma

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,140 +28,18 @@ void MainWindow::on_pushButton_clicked()
 {
 
     float op1 = ui->opD1->text().toFloat();
+
     float op2 = ui->opD2->text().toFloat();
 
-    ui->rD->setText(QString::fromStdString(std::to_string(op1+op2)));
+    float salida = aluAdd(op1,op2);
 
-    //Pasos previos
+    binaryWriteIn( ui->opB1, ConversorIEEE754::floattoIEESign(op1), ConversorIEEE754::floattoIEEExp(op1), ConversorIEEE754::floattoIEEMantisa(op1));
+    hexWriteIn(ui->opH1,  ConversorIEEE754::floattoIEESign(op1), ConversorIEEE754::floattoIEEExp(op1), ConversorIEEE754::floattoIEEMantisa(op1));
 
-    unsigned int signoA = ConversorIEEE754::floattoIEESign(op1);
-    unsigned int signoB = ConversorIEEE754::floattoIEESign(op2);
-
-    unsigned int expA = ConversorIEEE754::floattoIEEExp(op1);
-    unsigned int expB = ConversorIEEE754::floattoIEEExp(op2);
-
-    unsigned int manA = ConversorIEEE754::floattoIEEMantisa(op1) + bitPos.at(23);
-    unsigned int manB = ConversorIEEE754::floattoIEEMantisa(op2) + bitPos.at(23);
-
-
-    binaryWriteIn( ui->opB1, signoA, expA, manA);
-    binaryWriteIn( ui->opB2, signoB, expB, manB);
-    hexWriteIn(ui->opH1, signoA, expA, manA-bitPos.at(23));
-    hexWriteIn(ui->opH2, signoB, expB, manB-bitPos.at(23));
-
-    //Paso 1
-    unsigned int signoSuma;
-
-    unsigned int g = 0; unsigned int r = 0; unsigned int st = 0;
-    bool opChanged = false;
-    bool compP = false;
-
-    //Paso 2
-    if(expA < expB){
-        expA = ConversorIEEE754::floattoIEEExp(op2);
-        expB = ConversorIEEE754::floattoIEEExp(op1);
-        manA = ConversorIEEE754::floattoIEEMantisa(op2) + bitPos.at(23);
-        manB = ConversorIEEE754::floattoIEEMantisa(op1) + bitPos.at(23);
-        signoA = ConversorIEEE754::floattoIEESign(op2);
-        signoB = ConversorIEEE754::floattoIEESign(op1);
-        opChanged = true;
-    }
-    //Paso 3
-    unsigned int expR = expA;
-    unsigned int d = (expA - expB >= 0)? (expA - expB):(expB - expA);
-    //Paso 4
-
-    if(signoA!=signoB){
-        manB = (~manB)+1;
-    }
-
-    //Paso 5
-    unsigned int P = manB;
-    //Paso 6
-    if(d>=3 && d < 24){
-        g = ((bitPos.at(d-1)&P)!=0)? 1:0;
-        r = ((bitPos.at(d-2)&P)!=0)? 1:0;
-        st = ((bitPos.at(d-3)&P)!=0)? 1:0;
-    }
-    for(int i = d-3; i > 0 && !st; i--) st = st|(bitPos.at(d-i)&P);
-    //Paso 7
-    if(signoA!=signoB){
-        for(unsigned int i = 0; i < d; i++){
-            P >>= 1;
-            P += bitPos.at(23);
-        }
-    }
-    else P = P>>d;
-    //Paso 8
-    unsigned int C = 0;
-    C = calcularAcarreo(manA, P, 0, 0);
-    P = manA + P;
-
-    //Paso 9
-    if(signoA!=signoB && (P & bitPos.at(23)) != 0 && C == 0){
-        P = ~P+1;
-        compP = true;
-    }
-
-    //Paso 10
-    if(signoA == signoB && C==1){
-
-        st = g | r | st;
-
-        r = P%2;
-
-        P = P>>1;
-        P += bitPos.at(23);
-
-        expR++;
-
-    }
-    else{
-
-        int k = 0;
-        for(unsigned int aux = P; aux != 0 && (aux & bitPos.at(23)) == 0; aux <<=1) k++;
-        if (k == 0){
-            st = r|st;
-            r = g;
-        }
-        else{
-            st = 0; r = 0;
-        }
-
-        for(int i = 0; i < k; i++){
-
-            P<<=1;
-            P += g;
-
-        }
-
-        expR -= k;
-
-    }
-
-    //Paso 11:
-    if((r==1 && st == 1)||(r==1 && st == 0 && P%2)){
-        unsigned int aux = P;
-        P = P+1;
-        unsigned int C2 = P < aux;
-
-        if(C2) {
-            P >>= 1;
-            P += bitPos.at(23);
-            expR++;
-        }
-    }
-
-    //Paso 12:
-
-    signoSuma = (!opChanged && compP) ? signoB:signoA;
-
-    float salida = ConversorIEEE754::IEEtofloat(signoSuma, expR, P);
-
-    if((expA == 0 && manA-bitPos.at(23) == 0) || (expB == 0 && manB-bitPos.at(23) == 0)) salida = (expA == 0 && manA == 0)? ConversorIEEE754::IEEtofloat(signoB, expB, manB):ConversorIEEE754::IEEtofloat(signoA, expA, manA);
+    binaryWriteIn( ui->opB2,  ConversorIEEE754::floattoIEESign(op2), ConversorIEEE754::floattoIEEExp(op2), ConversorIEEE754::floattoIEEMantisa(op2));
+    hexWriteIn(ui->opH2, ConversorIEEE754::floattoIEESign(op2), ConversorIEEE754::floattoIEEExp(op2), ConversorIEEE754::floattoIEEMantisa(op2));
 
     ui->rD->setText(QString::fromStdString(std::to_string(salida)));
-
     binaryWriteIn(ui -> rB, ConversorIEEE754::floattoIEESign(salida), ConversorIEEE754::floattoIEEExp(salida), ConversorIEEE754::floattoIEEMantisa(salida));
     hexWriteIn(ui->rH, ConversorIEEE754::floattoIEESign(salida), ConversorIEEE754::floattoIEEExp(salida), ConversorIEEE754::floattoIEEMantisa(salida));
 
@@ -181,10 +59,8 @@ float MainWindow::aluAdd(float op1, float op2){
 
 
     const unsigned int excsBits = bitPos.at(31)+bitPos.at(30)+bitPos.at(29)+bitPos.at(28)+bitPos.at(27)+bitPos.at(26)+bitPos.at(25)+bitPos.at(24);
-    std::cout << "excsBits = " << excsBits << std::endl;
 
     //Paso 1
-    std::cout << "Paso1: "<<std::endl;
     unsigned int signoSuma;
 
     unsigned int g = 0; unsigned int r = 0; unsigned int st = 0;
@@ -192,7 +68,6 @@ float MainWindow::aluAdd(float op1, float op2){
     bool compP = false;
 
     //Paso 2
-    std::cout << "Paso2: ";
     if(expA < expB){
         expA = ConversorIEEE754::floattoIEEExp(op2);
         expB = ConversorIEEE754::floattoIEEExp(op1);
@@ -202,36 +77,25 @@ float MainWindow::aluAdd(float op1, float op2){
         signoB = ConversorIEEE754::floattoIEESign(op1);
         opChanged = true;
     }
-    std::cout <<" A = "<< signoA << expA -127 << manA - bitPos.at(23) <<", B = "<< signoB << expB -127 << manB - bitPos.at(23) << std::endl;
     //Paso 3
-    std::cout << "Paso3: ";
     unsigned int expR = expA;
     unsigned int d = expA - expB;
     d = (d>=0)? d:-d;
-    std::cout << d<< std::endl;
     //Paso 4
 
-    if(signoA!=signoB){
-        std::cout << "Paso4: ";
-        manB = (~manB)+1-excsBits;
-        std::cout <<"manB = "<<~(manB-1) - bitPos.at(23)-excsBits<<" ~manB = "<<manB -excsBits<< std::endl;
-    }
+    if(signoA!=signoB) manB = (~manB)+1-excsBits;
 
     //Paso 5
-    std::cout << "Paso5: ";
     unsigned int P = manB;
     std::cout << P<< std::endl;
     //Paso 6
-    std::cout << "Paso6: ";
     if(d>=3){
         g = (bitPos.at(d-1)&P)!=0;
         r = (bitPos.at(d-2)&P)!=0;
         st = (bitPos.at(d-3)&P)!=0;
     }
     for(int i = d-3; i > 0 && !st; i--) st = st|(bitPos.at(d-i)&P);
-    std::cout <<"g = "<< g <<", r = "<<r<<", st = "<<st<< std::endl;
     //Paso 7
-    std::cout << "Paso7: ";
     if(signoA!=signoB){
         for(unsigned int i = 0; i < d; i++){
             P >>= 1;
@@ -239,17 +103,13 @@ float MainWindow::aluAdd(float op1, float op2){
         }
     }
     else P = P>>d;
-    std::cout << P<< std::endl;
     //Paso 8
-    std::cout << "Paso8: " << " P = " << P << ", A = " << manA;
     unsigned int C = 0;
     C = calcularAcarreo(manA, P, 0, 0);
     P = manA + P;
-    std::cout << ", R = " << P << ", C = " << C << std::endl;
 
     //Paso 9
     if(signoA!=signoB && (P & bitPos.at(23)) != 0 && C == 0){
-        std::cout << "Paso9"<< std::endl;
         P = ~P+1;
         compP = true;
     }
@@ -258,8 +118,6 @@ float MainWindow::aluAdd(float op1, float op2){
     if(signoA == signoB && C==1){
 
         st = g | r | st;
-
-        std::cout << "Paso10a"<< std::endl;
 
         r = P%2;
 
@@ -272,9 +130,7 @@ float MainWindow::aluAdd(float op1, float op2){
     else{
 
         int k = 0;
-        std::cout << "Paso10b: P = "<< P;
         for(unsigned int aux = P; aux != 0 && (aux & bitPos.at(23)) == 0; aux <<=1) k++;
-        std::cout << " k = " << k<< std::endl;
         if (k == 0){
             st = r|st;
             r = g;
@@ -319,88 +175,23 @@ void MainWindow::on_pushButton_2_clicked()
 {
 
     float op1 = ui->opD1->text().toFloat();
+
     float op2 = ui->opD2->text().toFloat();
 
-    ui->rD->setText(QString::fromStdString(std::to_string(op1+op2)));
+    float salida = aluMultiply(op1,op2);
 
-    //Pasos previos
+    binaryWriteIn( ui->opB1, ConversorIEEE754::floattoIEESign(op1), ConversorIEEE754::floattoIEEExp(op1), ConversorIEEE754::floattoIEEMantisa(op1));
+    hexWriteIn(ui->opH1,  ConversorIEEE754::floattoIEESign(op1), ConversorIEEE754::floattoIEEExp(op1), ConversorIEEE754::floattoIEEMantisa(op1));
 
-    unsigned int signoA = ConversorIEEE754::floattoIEESign(op1);
-    unsigned int signoB = ConversorIEEE754::floattoIEESign(op2);
-
-    unsigned int expA = ConversorIEEE754::floattoIEEExp(op1);
-    unsigned int expB = ConversorIEEE754::floattoIEEExp(op2);
-
-    unsigned int manA = ConversorIEEE754::floattoIEEMantisa(op1) + bitPos.at(23);
-    unsigned int manB = ConversorIEEE754::floattoIEEMantisa(op2) + bitPos.at(23);
-
-
-    binaryWriteIn( ui->opB1, signoA, expA, manA);
-    binaryWriteIn( ui->opB2, signoB, expB, manB);
-    hexWriteIn(ui->opH1, signoA, expA, manA-bitPos.at(23));
-    hexWriteIn(ui->opH2, signoB, expB, manB-bitPos.at(23));
-
-    //Paso 1:
-    unsigned int signoR = signoA ^ signoB;
-    //Paso 2:
-    int expR = expA + expB - 127;
-
-    //Paso 3:
-    //Paso 3i:
-    unsigned int c = 0;
-    unsigned int P = 0;
-    unsigned int A = manA;
-
-    for(int i = 0; i < 24; i++){
-        P+=A%2*manB;
-        A = (A>>1) + (P%2)*bitPos.at(23);
-        P = (P>>1) + c*bitPos.at(23);
-        c>>=1;
-    }
-
-    //Paso 3ii:
-
-    if((P & bitPos.at(23))==0){
-        P <<= 1;
-    }
-    else{
-        expR++;
-    }
-
-    //Paso 3iii:
-    unsigned int r = (A & bitPos.at(23))!= 0;
-
-    //Paso 3iv:
-    unsigned int st = 0;
-    for(int i = 0; i < 23; i++) st |= (A & bitPos.at(i))!= 0;
-
-    //Paso 3v:
-    if((r&&st) ||(r&&!st&&P%2)){
-        P = P+1;
-    }
-
-    //DESBORDAMIENTOS
-    if(expR>0b11111111){
-
-        ui->rD->setText((signoR)? "-infinito":"infinito");
-        binaryWriteIn( ui->rB, signoA, expA, manA);
-
-    }
-    else if(expR<0){
-        unsigned int t = (1 - expR >= 0)? (1 - expR):(expR-0);
-        P >>= t;
-        expR = 0;
-    }
-
-    float salida = ConversorIEEE754::IEEtofloat(signoR, expR, P);
+    binaryWriteIn( ui->opB2,  ConversorIEEE754::floattoIEESign(op2), ConversorIEEE754::floattoIEEExp(op2), ConversorIEEE754::floattoIEEMantisa(op2));
+    hexWriteIn(ui->opH2, ConversorIEEE754::floattoIEESign(op2), ConversorIEEE754::floattoIEEExp(op2), ConversorIEEE754::floattoIEEMantisa(op2));
 
     ui->rD->setText(QString::fromStdString(std::to_string(salida)));
+    binaryWriteIn(ui -> rB, ConversorIEEE754::floattoIEESign(salida), ConversorIEEE754::floattoIEEExp(salida), ConversorIEEE754::floattoIEEMantisa(salida));
+    hexWriteIn(ui->rH, ConversorIEEE754::floattoIEESign(salida), ConversorIEEE754::floattoIEEExp(salida), ConversorIEEE754::floattoIEEMantisa(salida));
 
-    binaryWriteIn( ui->rB, signoR, expR, P);
-    hexWriteIn(ui->rH, signoR, expR, P-bitPos.at(23));
 
 }
-
 
 float MainWindow::aluMultiply(float op1, float op2){
 
@@ -413,23 +204,13 @@ float MainWindow::aluMultiply(float op1, float op2){
     unsigned int manA = ConversorIEEE754::floattoIEEMantisa(op1) + bitPos.at(23);
     unsigned int manB = ConversorIEEE754::floattoIEEMantisa(op2) + bitPos.at(23);
 
-    const unsigned int excsBits = bitPos.at(31)+bitPos.at(30)+bitPos.at(29)+bitPos.at(28)+bitPos.at(27)+bitPos.at(26)+bitPos.at(25)+bitPos.at(24);
-    std::cout << "excsBits = " << excsBits << std::endl;
-
     //Paso 1:
-    std::cout << "Paso1:";
     unsigned int signoR = signoA ^ signoB;
-    std::cout << "Signo = " << signoR << std::endl;
     //Paso 2:
-    std::cout << "Paso2:";
-    unsigned int expR = expA + expB - 127;
-    std::cout << "Exponente = " << expR-127 << std::endl;
+    unsigned int expR = expA + expB - 0b1111111;
 
     //Paso 3:
-    std::cout << "Paso3:"<<std::endl;
-
     //Paso 3i:
-    std::cout << "  Paso3i:";
     unsigned int c = 0;
     unsigned int P = 0;
     unsigned int A = manA;
@@ -441,10 +222,7 @@ float MainWindow::aluMultiply(float op1, float op2){
             c>>=1;
     }
 
-    std::cout << " P = " << P << " A = "<< A<< std::endl;;
-
     //Paso 3ii:
-    std::cout << "  Paso3ii:";
 
     if((P & bitPos.at(23))==0){
         P <<= 1;
@@ -452,31 +230,32 @@ float MainWindow::aluMultiply(float op1, float op2){
     else{
         expR++;
     }
-    std::cout << " P = " << P << " expR = " << expR << std::endl;
 
     //Paso 3iii:
-    std::cout << "  Paso3iii:";
     unsigned int r = (A & bitPos.at(23))!= 0;
-    std::cout << " r = " << r << std::endl;
 
     //Paso 3iv:
-    std::cout << "  Paso3iv:";
     unsigned int st = 0;
     for(int i = 0; i < 23; i++) st |= (A & bitPos.at(i))!= 0;
-    std::cout << " st = " << st << std::endl;
 
     //Paso 3v:
-    std::cout << "  Paso3v:";
     if((r&&st) ||(r&&!st&&P%2)){
         P = P+1;
     }
-    std::cout << " P = " << P << std::endl;
 
     //DESBORDAMIENTOS
     if(expR>0b11111111){
-        return Q_INFINITY;
+        return (signoR)? -Q_INFINITY:Q_INFINITY;
     }
-    else if(expR<0);
+    else if(expR<0){
+
+        unsigned int t = 1 - expR;
+
+        P >>=t;
+
+        expR = 0;
+
+    }
     else{
         float salida = ConversorIEEE754::IEEtofloat(signoR, expR, P);
         return salida;
